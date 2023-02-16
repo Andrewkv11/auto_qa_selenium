e0fde886e89dcd2c5ef9b2014eb00832183160a3
Andrewkv11 <andko4anov@yandex.ru>
2023-02-16 21:39:17 +0400

    added test TestMenu

--- tests/widgets_test.py
+++ tests/widgets_test.py
@@ -1,7 +1,7 @@
 import time
 
 from pages.widgets_page import AccordianPage, AutoCompletePage, DatePickerPage, ProgressBarPage, SliderPage, TabsPage, \
-    ToolTipsPage
+    ToolTipsPage, MenuPage
 
 
 class TestWidgets:
@@ -92,3 +92,12 @@ class TestWidgets:
             assert field_text == "You hovered over the Button", "hover missing or incorrect content"
             assert contrary_text == "You hovered over the text field", "hover missing or incorrect content"
             assert section_text == "You hovered over the Contrary", "hover missing or incorrect content"
+
+    class TestMenu:
+
+        def test_menu_items(self, driver):
+            menu_page = MenuPage(driver, 'https://demoqa.com/menu')
+            menu_page.open()
+            data = menu_page.check_menu()
+            assert data == ['Main Item 1', 'Main Item 2', 'Sub Item', 'Sub Item', 'SUB SUB LIST »', 'Sub Sub Item 1',
+                            'Sub Sub Item 2', 'Main Item 3'], "menu items do not exists or have not been selected"
